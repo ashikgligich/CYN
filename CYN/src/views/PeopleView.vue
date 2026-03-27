0bb1dca9694afc4669631c1d6e97119b6f9acfe0
LeoDwi <leod13579@gmail.com>
2026-03-27 00:11:07 -0400

    temp view to hold list of people maybe

---
+++ CYN/src/views/PeopleView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue'
+import Portfolio from '../components/Portfolio.vue'
+const people = ref([
+  { name: 'John Doe', role: 'Developer' },
+  { name: 'Jane Smith', role: 'Designer' },
+])
+</script>
+
+<template>
+  <main>
+    <Portfolio v-for="person in people" :key="person.name" :person="person" />
+  </main>
+</template>
